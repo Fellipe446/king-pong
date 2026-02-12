72d2ab82879dd539b62c176793a89776926a914e
Fellipe446 <fellipejose951@gmail.com>
2026-02-11 22:14:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const http = require('http');
 const path = require('path');
 const app = express();
 const server = http.createServer(app);
+
 app.use(express.static(__dirname));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`[KING DEV ACADEMY] Arena Online.`));
+server.listen(PORT, () => console.log(`[KING DEV ACADEMY] Servidor ativo em: http://localhost:${PORT}`));
