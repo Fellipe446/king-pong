8dad66625c6598c714f54be28619feef1a5a71ef
Fellipe446 <fellipejose951@gmail.com>
2026-02-11 22:21:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const http = require('http');
 const path = require('path');
 const app = express();
 const server = http.createServer(app);
-
 app.use(express.static(__dirname));
-
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`[KING DEV ACADEMY] Servidor ativo em: http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`[KING DEV ACADEMY] Online em: http://localhost:${PORT}`));
