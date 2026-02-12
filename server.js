a316f2fea7c0b36bee438134cb1cbcf1bd7c65e6
Fellipe446 <fellipejose951@gmail.com>
2026-02-11 22:28:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const server = http.createServer(app);
 app.use(express.static(__dirname));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`[KING DEV ACADEMY] Sistema operacional.`));
+server.listen(PORT, () => console.log(`[KING DEV ACADEMY] Arena Pronta.`));
